d194648c4c07a3642ef765a9bee23e279092b89a
RaghavendraKarrothu <raghavendra.karrothu@gmail.com>
2021-06-24 12:47:18 +0530

    pop and remove functions added

--- linked_list.py
+++ linked_list.py
@@ -26,14 +26,90 @@ class LinkedList:
         self.count += 1
 
     def insert(self, _data, pos):
-        pass
-
+        new_node = Node(_data)
+        if pos <= 0:
+            new_node.next = self.head
+            self.head = new_node
+        elif pos >= self.count:
+            self.tail_next = new_node
+            self.tail = new_node
+        else:
+            tmp = self.head
+            for _ in range(pos-1):
+                tmp = tmp.next
+            new_node.next = tmp.next
+            tmp.next = new_node
+            
+            
     def pop(self, pos=None):
-        pass
+        
+        if pos == None:
+            tmp = self.head
+            for _ in range(self.count-2):
+                tmp = tmp.next
+            tmp1 = tmp.next
+            self.tail = tmp
+            tmp.next = None
+        elif 0 < pos < self.count:
+            tmp = self.head
+            for _ in range(pos-1):
+                tmp = tmp.next
+            tmp1 = tmp.next
+            tmp.next = tmp1.next
+        elif pos <= 0:
+            tmp = self.head
+            tmp1 = self.head
+            self.head = tmp1.next
+            tmp.next = tmp1.next
+        else:
+            tmp = self.head
+            for _ in range(self.count-2):
+                tmp = tmp.next
+            tmp1 = tmp.next
+            self.tail = tmp
+            tmp.next = None
+        data = tmp1.data
+        del tmp1
+        self.count -= 1
+        return data
 
     def remove(self, _data):
-        pass
+        tmp0 = self.head
+        tmp = self.head
+        pos = 0
+        is_data_exist = False
+        for _ in range(self.count):
+            if tmp0.data == _data:
+                is_data_exist = True
+                break
+            tmp0 = tmp0.next
+            pos += 1
 
+        for _ in range(pos - 1):
+            tmp = tmp.next
+        if is_data_exist:
+            if pos == 0:
+                tmp1 = self.head
+                self.head = tmp1.next
+                tmp.next = tmp1.next
+                del tmp1
+                self.count -= 1
+            elif 0 < pos < self.count:
+                tmp1 = tmp.next
+                tmp.next= tmp1.next
+                del tmp1
+                self.count -= 1
+            else:
+                tmp1 = tmp
+                self.tail = tmp
+                tmp.next = None
+                del tmp1
+                self.count -= 1
+
+            
+        return
+
+            
     def __str__(self):
         _head = self.head
         s = "head -> "
@@ -52,9 +128,12 @@ class LinkedList:
             l.append(str(_head.data))
             _head = _head.next
 
-        return f"LinkedList([{', '.join(l)}])"
+        return "LinkedList({})".format([', '.join(l)])
 
 
 if __name__ == '__main__':
     ll = LinkedList([5, 6, 2, 4, 8, 1, 9])
+    # ll.pop()
+    # ll.pop(4)
+    ll.remove(5)
     print(ll)
